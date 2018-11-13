444c7e4ea7d9af0f2cd1cf9d25ddf59723760dba
Matt Stuhring <whereyouatmatt@gmail.com>
2018-11-13 11:35:16 -0800

    Completed javascript unit testing course

---
+++ battleship-unit-tests/game-logic/game-instance.js
@@ -0,0 +1,16 @@
+const fire = require('./ship-methods.js').fire;
+
+function checkGameStatus(players) {
+  return false;
+}
+
+function takeTurn(opposingPlayer, guessFunction) {
+  let coordinates = guessFunction();
+  fire(opposingPlayer, coordinates);
+  let gameOver = checkGameStatus();
+
+  return gameOver;
+}
+
+module.exports.checkGameStatus = checkGameStatus;
+module.exports.takeTurn = takeTurn;
---
+++ battleship-unit-tests/test/game.spec.js
@@ -0,0 +1,61 @@
+const expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function () {
+  describe('checkGameStatus', function () {
+    const checkGameStatus = require('../game-logic/game-instance.js').checkGameStatus;
+    it('should tell me when the game is over', function () {
+      let players = [
+        {
+					ships: [
+						{
+							locations: [[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+        }
+      ];
+      let actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    });
+  });
+
+  describe('takeTurn', function() {
+    const takeTurn = require('../game-logic/game-instance.js').takeTurn;
+    let guess;
+
+    beforeEach(function() {
+      guess = function() { return [0, 0]; };
+      player = {
+        ships: [
+          {
+            locations: [[0, 0]],
+            damage: []
+          }
+        ]
+      }
+    });
+
+    it('should return false if the game ends', function() {
+      let actual = takeTurn(player, guess);
+      expect(actual).to.be.false;
+    });
+  });
+
+  function saveGame(callback) {
+    setTimeout(function() {
+      callback();
+    }, 1000);
+  }
+
+  describe('saveGame', function() {
+    it('should update save status', function(done) {
+      let status = 'game not saved...';
+
+      saveGame(function() {
+        status = 'game saved!';
+        expect(status).to.equal('game saved!');
+        done();
+      });
+    });
+  });
+});
